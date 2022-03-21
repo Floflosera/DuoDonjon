285f44c422c35021cbd336db4212cc2444d7a4bb
Floflosera <floflosera@live.fr>
2022-03-21 13:29:18 +0100

    Combat 1 qui marche

--- Scenes/Ennemi1_1.gd
+++ Scenes/Ennemi1_1.gd
@@ -9,9 +9,9 @@ onready var blinde = 0
 
 func _ready():
 	#Statistiques de l'ennemi
-	pvmax = 800
-	pv = 800
-	defense = 15
+	pvmax = 200
+	pv = 200
+	defense = 30
 	vitesse = 3
 	
 	#Ne cible que Harry, car il est le seul à participer au combat
@@ -44,30 +44,36 @@ func degatsPrisDef(degats):
 
 func choixSkill():
 	if(aBD.pv > 0 && aBG.pv > 0):
-		choixSkill = randi() % 2
+		if(combat.nTour == 1):
+			choixSkill = 1
+		else:
+			choixSkill = randi() % 2
 	elif(aBD.pv > 0 || aBG.pv > 0):
 		choixSkill = 0
 	else:
 		choixSkill = 2
 	
 	tourEffectue = false
-	
+
+#Coup de boulet
 func castSkill1():
-	cible.degatsPrisDef(80 + randi()%9)
+	cible.degatsPrisDef(60 + randi()%7)
 	yield(cible,"degatsTermine")
 	
 	emit_signal("skillCast")
 
+#Commotion
 func castSkill2():
-	cible.degatsPrisDef(80 + randi()%9)
+	cible.degatsPrisDef(70 + randi()%8)
 	yield(cible,"degatsTermine")
 	blinde = 2
 	secondText = true
 	
 	emit_signal("skillCast")
 
+#Coup de boule
 func castSkill3():
-	cible.degatsPrisDef(80 + randi()%9)
+	cible.degatsPrisDef(40 + randi()%5)
 	yield(cible,"degatsTermine")
 	
 	emit_signal("skillCast")
--- Scenes/Ennemi1_3.gd
+++ Scenes/Ennemi1_3.gd
@@ -1,8 +1,35 @@
 extends "Ennemi.gd"
 
+onready var aRobotGardien = get_node("../Ennemi1_1")
+
 func _ready():
 	#Statistiques de l'ennemi
-	pvmax = 800
-	pv = 800
-	defense = 15
-	vitesse = 3
+	pvmax = 70
+	pv = 70
+	defense = 0
+	vitesse = 0
+
+#Surcharge pour envoyer des infos au boss
+func degatsPris(degats):
+	spriteAnim.play("Blesse")		#Lance l'animation des dégâts pris
+	if(degats <= 1):
+		degats = 1
+	if(pv - degats <= 0):			#La condition fait en sorte de ne pas avoir des pv négatifs
+		pv = 0						#Si les pv sont inférieurs aux dégâts réçus, alors on tombe à 0pv
+		tourEffectue = true			#Si un allié n'a plus de pv, alors son tour sera compté comme déjà passé
+	else:
+		pv -= degats				#Sinon les dégâts sont soustraits aux pv du personnage
+	barreVie.value = pv
+	yield(spriteAnim,"animation_finished")	#Attend la fin de l'animation de blessure
+	changerSprite()							#Change le sprite des 2 persos
+	
+	#informations de base pour l'animation du richText qui montre les dégâts
+	#à concaténer avec le nombre des dégâts quand on inflige les dégâts avec un personnage
+	showDegats.set_bbcode("[center][wave freq=25]")
+	
+	if(pv == 0 && aRobotGardien.BD.pv == 0):
+		aRobotGardien.choixSkill = 2
+	elif(pv == 0):
+		aRobotGardien.choixSkill = 0
+	
+	emit_signal("degatsTermine")
